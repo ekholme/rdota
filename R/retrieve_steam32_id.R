c57c36c1dc9b4439fdf72320c044404685fb40f2
Eric Ekholm <eric.ekholm@gmail.com>
2021-05-11 06:04:39 -0400

    fix retrieve_steam32_id by using character string instead of numeric

--- R/retrieve_steam32_id.R
+++ R/retrieve_steam32_id.R
@@ -1,43 +1,38 @@
 #' Retrieve 32bit Steam ID
 #' 
-#' The OpenDota API requires a 32-bit Steam ID for player queries. This helper function will retrieve a 32-bit ID if provided a 64-bit ID
+#' The OpenDota API requires a 32-bit Steam ID for player queries. This helper function will retrieve a 32-bit ID using the website steamidfinder.com
 #'
-#' @param long_id The 64-bit Steam ID of the player. Must be numeric.
+#' @param long_id The 64-bit Steam ID of the player. Must be passed in as a string
 #'
 #' @return
 #' @export
 #'
 #' @examples \dontrun{
-#' retrieve_steam32_id(76561198069153050)
+#' retrieve_steam32_id("76561198069153050")
 #' }
 retrieve_steam32_id <- function(long_id) {
   
-  if (!is.numeric(long_id)) {
-    rlang::abort(paste0("`long_id` must be your Steam 64 bit ID number, not a ", typeof(long_id)))
+  if (!is.character(long_id)) {
+    rlang::abort(paste0("`long_id` must be your Steam 64 bit ID passed in as a string, not as a ", typeof(long_id)))
   }
   
-  url <- 'https://steamid.xyz/'
+  idfinder <- 'https://steamidfinder.com/lookup/'
   
-  site <- xml2::read_html(url)
+  site <- xml2::read_html(paste0(idfinder, long_id))
   
-  form <- rvest::html_elements(site, "form")
+  out <- rvest::html_elements(site, "div")
+  #note it's the 16th element of this list that we want
   
-  form <- rvest::html_form(form[[1]])
-  
-  vals <- rvest::html_form_set(form, id = long_id)
-  
-  session <- rvest::html_session(url)
-  
-  sub <- rvest::session_submit(session, vals)
-  
-  txt <- rvest::html_text(rvest::html_elements(sub, "#guide"))
+  txt <- rvest::html_text(out[[15]])
   
   lst <- stringr::str_split(txt, "\n")
   
-  el <- lst[[1]][[24]]
+  el <- lst[[1]][[5]]
   
-  ret <- as.integer(stringr::str_remove_all(el, "\\D+"))
+  tmp <- stringr::str_remove_all(el, "\\D+")
+  
+  ret <- stringr::str_sub(tmp, start = 3L)  
   
   return(ret)
-  
 }
+
