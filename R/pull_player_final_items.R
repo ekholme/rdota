ebc92b0644aed9bb0050696eeacbe244ce30f4ce
Eric Ekholm <eric.ekholm@gmail.com>
2021-08-30 07:18:37 -0400

    finish pull_player_final_items func

--- R/pull_player_final_items.R
+++ R/pull_player_final_items.R
@@ -1,35 +1,53 @@
 
-ind <- c(paste0("item_", 0:5), "item_neutral", paste0("backpack_", 0:3))
 
-o <- purrr::map(ind, ~purrr::pluck(mt, "players", 1, 1, .x))
-
-t <- tibble::tibble(
-  item_slot = ind,
-  item = purrr::map_int(o, ~replace_null(.x, NA_integer_))
-)
-#so, the above will work for a single player. Now we want to get them for each player
 
 get_indiv_player_items <- function(obj, player_num) {
-  
+
   ind <- c(paste0("item_", 0:5), "item_neutral", paste0("backpack_", 0:3))
-  
+
   o <- purrr::map(ind, ~purrr::pluck(obj, "players", 1, player_num, .x))
   
+  match_id <- obj$match_id
+  
+  player_slot <- purrr::pluck(obj, "players", 1, player_num, "player_slot")
+
   account_id <- purrr::pluck(obj, "players", 1, player_num, "account_id")
   
+  account_id <- replace_null(account_id, NA_integer_)
+
   tibble::tibble(
+    match_id = match_id,
     account_id = account_id,
+    player_slot = player_slot,
     item_slot = ind,
     item = purrr::map_int(o, ~replace_null(.x, NA_integer_))
   )
-  
+
 }
 
-aa <- get_indiv_player_items(mt, 1)
 
+#' TODO
+#'
+#' @param obj 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 pull_player_final_items <- function(obj) {
   
-  purrr::map(1:10, ~get_indiv_player_items(obj = obj, player_num = .x))
+  cl <- class(obj)
   
+  if (!((!"rdota_match" %in% cl) | (!"match_tbl" %in% cl))) {
+    rlang::abort(paste0("`pull_player_final_items` expects an object of class 'rdota_match' or 'match_tbl', not ", class(obj)))
+  }
+  
+  obj <- if ("rdota_match" %in% class(obj) & (!"match_tbl" %in% class(obj))) {
+    tidy_rdota_match(obj)
+  } else obj
+
+  t <- purrr::map(1:10, ~get_indiv_player_items(obj = obj, player_num = .x))
+  
+  dplyr::bind_rows(t)
+
 }
-#not sure what's going on here -- resume here later
